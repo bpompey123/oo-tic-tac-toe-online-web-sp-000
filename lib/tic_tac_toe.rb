76b2f28265ac44fdbe6ff759ae2050a743ea66e7
Brian Pompey <brianthescholar@gmail.com>
2019-09-27 00:51:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,14 @@ class TicTacToe
 
   end
 
-  
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+
+    end
+  end
 
 
 
