1341e81cb5b9979e36206d8323d1326f36f602ba
Brian Pompey <brianthescholar@gmail.com>
2019-09-27 01:07:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,7 +87,7 @@ class TicTacToe
       position_3 = @board[win_index_3]
 
 
-      position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      position_1 == position_2 && position_2 == position_3 && position_taken?(win_index_1)
 
     end
   end
