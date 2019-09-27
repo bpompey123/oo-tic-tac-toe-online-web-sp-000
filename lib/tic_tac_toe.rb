dc33ebd5ec73088a5c4f574ab99b53a93b992fa7
Brian Pompey <brianthescholar@gmail.com>
2019-09-27 01:14:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,7 +118,7 @@ class TicTacToe
     end
     if won?
       winner == "X" || winner == "O"
-      puts "Congratulations #{winner(board)}!"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
